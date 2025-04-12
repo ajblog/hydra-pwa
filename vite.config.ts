7d943516a59f92d29ae9c1dbf02e15e2aa50ac60
ajafarian <a.jafarian@revotech.com.au>
2025-04-12 12:35:55 +0330

    fixed manifest issue

--- vite.config.ts
+++ vite.config.ts
@@ -15,30 +15,12 @@ export default defineConfig({
         description: "Your smart assistant on the seas",
         theme_color: "#ffffff",
         icons: [
-          // {
-          //   src: "/assets/Hydra-Logo-192.png", // Fallback PNG
-          //   sizes: "192x192",
-          //   type: "image/png",
-          //   purpose: "any",
-          // },
-          // {
-          //   src: "/assets/Hydra-Logo-512.png", // Fallback PNG
-          //   sizes: "512x512",
-          //   type: "image/png",
-          //   purpose: "any",
-          // },
           {
-            src: "/assets/Hydra-Logo.svg", // SVG icon
-            sizes: "any", // SVGs are scalable
-            type: "image/svg+xml",
+            src: "/assets/Logo-png.png",
+            sizes: "61x74", // Small icon
+            type: "image/png",
             purpose: "any",
           },
-          // {
-          //   src: "/assets/Hydra-Logo-Maskable.png", // Optional maskable icon
-          //   sizes: "512x512",
-          //   type: "image/png",
-          //   purpose: "maskable",
-          // },
         ],
       },
       devOptions: {
