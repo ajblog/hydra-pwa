bd09898de32ba9cf1d2ba1318bad7b42179422fa
ajafarian <a.jafarian@revotech.com.au>
2025-05-11 13:12:00 +0330

    made sign up two steps

--- src/views/SignUp/SignUp.tsx
+++ src/views/SignUp/SignUp.tsx
@@ -26,6 +26,8 @@ export const SignUp = () => {
   const navigate = useNavigate();
   const [focusedElement, setFocusedElement] = useState<boolean>(false);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [step, setStep] = useState<1 | 2>(1);
+  const [step1Data, setStep1Data] = useState<FieldValues>({});
 
   useEffect(() => {
     const handleFocus = (event: FocusEvent) => {
@@ -39,7 +41,8 @@ export const SignUp = () => {
       document.removeEventListener("focusin", handleFocus);
     };
   }, []);
-  const signUpFields = [
+
+  const fieldsStep1 = [
     {
       name: "firstname",
       type: "text",
@@ -81,6 +84,9 @@ export const SignUp = () => {
       validation: { required: "نام سازمان الزامی است" },
       icon: <OrganizationIcon />,
     },
+  ];
+
+  const fieldsStep2 = [
     {
       name: "username",
       type: "text",
@@ -113,12 +119,20 @@ export const SignUp = () => {
     },
   ];
 
-  const handleSubmit = async (e: FieldValues) => {
-    if ("phonenumber" in e) {
-      e.phonenumber = `+98${e.phonenumber}`;
+  const handleStep1Submit = (data: FieldValues) => {
+    setStep1Data(data);
+    setStep(2);
+  };
+
+  const handleFinalSubmit = async (data: FieldValues) => {
+    const fullData = { ...step1Data, ...data };
+
+    if ("phonenumber" in fullData) {
+      fullData.phonenumber = `+98${fullData.phonenumber}`;
     }
+
     try {
-      const res = await signupApi(e);
+      const res = await signupApi(fullData);
       if (res.token) {
         setCookie("access_token", res.token.access, { minutes: 15 });
         setCookie("refresh_token", res.refresh, { days: 7 });
@@ -136,8 +150,10 @@ export const SignUp = () => {
     }
     localStorage.setItem("hasVisited", "true");
   };
+
   if (isLoggedIn)
     return <SuccessLoginPage title=" ثبت نام با موفقیت انجام شد" />;
+
   return (
     <div>
       <div className="pr-10 pt-10 flex flex-col justify-between">
@@ -156,59 +172,70 @@ export const SignUp = () => {
           exit={{ scale: 0.8, opacity: 0 }}
           transition={{ duration: 0.9, ease: "easeOut", delay: 0.2 }}
         >
-          {!focusedElement && (
-            <p className="text-white font-bold text-xl max-w-3xs mt-4 leading-16">
-              حساب کـــاربری
-              <br /> خود را <br /> ایجاد کنید!
-            </p>
-          )}
+          <p className="text-white font-bold text-2xl max-w-3xs mt-4 leading-16">
+            ثبت اطلاعات
+            <br />
+            کاربری
+          </p>
         </motion.div>
       </div>
+
       <motion.div
         initial={{ opacity: 0.5, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.7 }}
-        className={`absolute h-[calc(100%-200px)] right-0 w-full rounded-full bg-[#5b55edE6] -z-[10] ${
-          focusedElement ? "top-[100%]" : "top-[200px]"
-        }`}
-      ></motion.div>
+        className={`absolute h-[calc(100%-200px)] right-0 w-full rounded-full bg-[#5b55edE6] -z-[10] ${"top-[200px]"}`}
+      />
+
       <div className="bg-white fixed inset-x-0 bottom-0 rounded-t-4xl flex max-h-[80vh] flex-col justify-between pt-8 px-8 pointer-events-auto overflow-visible transition-all duration-1000 ease-out">
         <img
           src={signupPeople}
           alt="signup-sign"
-          className={`absolute top-[-146px] z-[60] w-fit transition-all duration-1000 ${
-            focusedElement ? "-left-[100%]" : "left-[3%]"
-          }`}
+          className={`absolute top-[-146px] z-[60] w-fit transition-all duration-1000 ${"left-[3%]"}`}
         />
 
-        <Form
-          fields={signUpFields}
-          onSubmit={(e) => handleSubmit(e)}
-          hideButton={!focusedElement}
-          buttonTheme="default"
-          inputTheme="dark"
-          submitText="ثبت نام"
-          className="overflow-y-auto"
-          customLink={
-            focusedElement && (
-              <motion.div
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.7 }}
-                className="flex justify-center items-center col-span-full"
-              >
-                <p className="text-black text-sm">قبلا ثبت نام کرده اید؟</p>
-                <Button
-                  onClick={() => navigate("/sign-in")}
-                  className="font-bold text-black text-sm"
-                  variant="link"
+        {step === 1 && (
+          <Form
+            fields={fieldsStep1}
+            onSubmit={handleStep1Submit}
+            hideButton={!focusedElement}
+            buttonTheme="default"
+            inputTheme="dark"
+            submitText="ادامه"
+            className="overflow-y-auto"
+          />
+        )}
+
+        {step === 2 && (
+          <Form
+            fields={fieldsStep2}
+            onSubmit={handleFinalSubmit}
+            hideButton={!focusedElement}
+            buttonTheme="default"
+            inputTheme="dark"
+            submitText="ثبت نام"
+            className="overflow-y-auto"
+            customLink={
+              focusedElement && (
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: 0.7 }}
+                  className="flex justify-center items-center col-span-full"
                 >
-                  وارد شوید.
-                </Button>
-              </motion.div>
-            )
-          }
-        />
+                  <p className="text-black text-sm">قبلا ثبت نام کرده اید؟</p>
+                  <Button
+                    onClick={() => navigate("/sign-in")}
+                    className="font-bold text-black text-sm"
+                    variant="link"
+                  >
+                    وارد شوید.
+                  </Button>
+                </motion.div>
+              )
+            }
+          />
+        )}
       </div>
     </div>
   );
