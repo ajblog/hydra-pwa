2d68b90dc2a0e4c565068688408207b65a1e425c
Iman <imankeramati1401@gmail.com>
2025-06-01 15:45:49 +0330

    -handle sticky title

--- src/components/molecules/DaySection/DaySection.tsx
+++ src/components/molecules/DaySection/DaySection.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useRef } from "react";
 import { WeatherCard } from "../WeatherCard/WeatherCard";
+import { DaySectionPropTypes } from "./DaySection.tyes";
 
 function DaySection({
   day,
@@ -8,20 +10,54 @@ function DaySection({
   nextDay,
   selectedCardId,
   setSelectedCardId,
+  setVisibleDay,
+  scrollContainerRef,
   dayIndex,
-}: {
-  day: any;
-  isFirstDay: boolean;
-  startTime: number;
-  selectedCardId: string | null;
-  setSelectedCardId: (id: string) => void;
-  dayIndex: number;
-  nextDay?: any;
-}) {
+  previousDay,
+}: DaySectionPropTypes) {
+  const dayRef = useRef<HTMLHeadingElement | null>(null);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (!dayRef.current || !scrollContainerRef.current) return;
+
+      const rect = dayRef.current.getBoundingClientRect();
+      const containerRect = scrollContainerRef.current.getBoundingClientRect();
+
+      const leftDistance = rect.left - containerRect.left;
+
+      if (leftDistance <= 0) {
+        console.log(day.day_name);
+        setVisibleDay(day.day_name);
+      } else if (leftDistance >= 0 && leftDistance < 80) {
+        setVisibleDay("");
+      }
+
+      if (previousDay && leftDistance > 80 && leftDistance < 300) {
+        setVisibleDay(previousDay.day_name);
+      }
+    }
+
+    const container = scrollContainerRef.current;
+    if (container) {
+      container.addEventListener("scroll", handleScroll, { passive: true });
+      handleScroll();
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, [day.day_name, previousDay, scrollContainerRef, setVisibleDay]);
+
   return (
-    <div className=" ltr relative w-full">
-      {/* Sticky day name on the left */}
-      <h2 className="z-30 w-fit sticky -left-16 bottom-0 ltr bg-white font-bold text-left mb-2.5 py-1 px-2 border-r border-gray-300">
+    <div className=" ltr  w-full">
+      <h2
+        ref={dayRef}
+        style={{ minWidth: 80 }}
+        className="z-30 w-fit sticky left-0 bottom-0 ltr text-gray-700 bg-white font-bold text-center mb-2.5 py-1 px-2 border-r border-gray-300"
+      >
         {day.day_name}
       </h2>
       <div className="flex ltr overflow-visible pb-0.5">
--- src/components/organs/AllInOneChart.tsx
+++ src/components/organs/AllInOneChart.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ClockIcon } from "lucide-react";
 import { CustomChart, DaySection } from "../molecules";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const chartParameters = [
   {
-    name: "ساعت",
+    name: "زمان",
     icon: "",
   },
   {
@@ -29,15 +29,33 @@ const AllInOneChart = ({
   data: any;
   startTime?: string;
 }) => {
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+
   const [selectedCardId, setSelectedCardId] = useState<string | null>(null);
+  const [visibleDay, setVisibleDay] = useState<string | null>(null);
 
   const temperatures = data.flatMap((day: any) =>
     day.weather_info.map((info: any) => info.temperature.temperature)
   );
   return (
     <div className="relative w-full h-full overflow-hidden">
-      <div className="flex flex-row-reverse overflow-x-auto relative">
-        <div className="bg-gray-200 sticky -left-1 rounded-xl p-1 mt-10 shrink-0 z-50">
+      <div
+        className="z-40 absolute -left-1 text-center text-gray-700 h-fit top-0 ltr bg-white font-bold mb-2.5 py-1 px-2 border-r border-gray-300"
+        style={{
+          minWidth: 80,
+          opacity: visibleDay && visibleDay.length > 0 ? 1 : 0,
+          transition: "opacity 0.4s",
+          pointerEvents: visibleDay && visibleDay.length > 0 ? "auto" : "none", // optional: disables interaction when hidden
+        }}
+      >
+        {visibleDay}
+      </div>
+
+      <div
+        ref={scrollContainerRef}
+        className="flex flex-row-reverse overflow-x-auto relative"
+      >
+        <div className="bg-[#5b55ed] sticky -left-1 rounded-xl p-1 mt-10 shrink-0 z-50">
           {chartParameters.map((item, index) => (
             <div
               key={index}
@@ -45,14 +63,16 @@ const AllInOneChart = ({
                 index === 1 ? "pb-20 pt-2" : ""
               }`}
             >
-              <div className=" bg-gray-200 z-10">
+              <div className="  z-10">
                 {index === 0 ? (
-                  <ClockIcon size={12} />
+                  <ClockIcon size={12} color="white" />
                 ) : (
-                  <span className="text-xs">{item.icon}</span>
+                  <span className="text-xs text-white">{item.icon}</span>
                 )}
               </div>
-              <span className="text-xs whitespace-nowrap">{item.name}</span>
+              <span className="text-xs whitespace-nowrap text-white">
+                {item.name}
+              </span>
             </div>
           ))}
         </div>
@@ -71,10 +91,13 @@ const AllInOneChart = ({
                 key={index}
                 day={day}
                 nextDay={data[index + 1]}
+                previousDay={data[index - 1]}
                 isFirstDay={index === 0}
                 selectedCardId={selectedCardId}
                 setSelectedCardId={setSelectedCardId}
+                setVisibleDay={setVisibleDay}
                 dayIndex={index}
+                scrollContainerRef={scrollContainerRef}
                 startTime={
                   startTime.split(":")[0]
                     ? parseInt(startTime.split(":")[0])
